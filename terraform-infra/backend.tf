be5fc2a235407f7c8f917e9b54c81210eb4d0767
Janith Wathsala <janijaniwathsala@gmail.com>
2026-02-04 22:56:32 +0530

    add code

---
+++ terraform-infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "devops-lab-terraform-state-janith"
+    key            = "devops-lab/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
